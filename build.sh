5d575afc1d9dab9c952901d75b027382bedd3e5d
Tommaso Sorgente <tommaso.sorgente@ge.imati.cnr.it>
2025-08-08 18:18:04 +0200

    CGAL build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to this script's directory
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CGAL_path="$scriptDir/CGAL-apps"
+output_path="../../code"
+
+echo "compiling CGAL apps"
+cd $CGAL_path
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$output_path -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=$output_path 
+make
+
+echo "done!"
